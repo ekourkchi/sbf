a6b2f6b2f2c6e31087e059912a884a0026de6dbb
ekourkchi <ekourkchi@gmail.com>
2024-09-27 13:27:31 -0700

    update run docker for Mac systems

--- run_docker.sh
+++ run_docker.sh
@@ -1,33 +1,53 @@
 #!/bin/bash
 
 
-docker pull astromatrix/sbf_tools:v1.0
+export NOTEBOOKS_FOLDER="notebooks"
+export NOTEBOOK_IP="8888"
 
+
+
+
+docker pull astromatrix/notebook:v1.0
 
 #  ## Windows
 # export display=$(grep -oP '(?<=nameserver\s)\d+\.\d+\.\d+\.\d+' /etc/resolv.conf):0
 
 # ## MAC
 # export display=192.168.0.6:0
+ip_address=$(ifconfig | grep -A 1 'en' | grep 'inet ' | awk '{print $2}')
+export display=$ip_address:0
 
 # ## Linux
-export display=$DISPLAY
+# export display=$DISPLAY
 
 xhost +local:docker 
 
+export containerName="sbf_container"
+
+# Check if a container named "container" exists
+container_id=$(docker ps -aq -f name="^$containerName$")
+
+if [ -n "$container_id" ]; then
+  echo "Container named '$containerName' found. Removing..."
+  docker rm -f "$container_id"
+  echo "Container removed."
+else
+  echo "Generating container '$containerName' ..."
+fi
+
+
 docker run -it --rm \
     --memory="2000M" \
-    -p 8888:8888 \
+    -p $NOTEBOOK_IP:8888 \
     -v $(pwd)/pysbf:/home/sbf/pysbf:ro \
-    -v /home/ehsan/PanStarrs/Jan/HI/augment/SBF/codes/notebooks:/home/sbf/notebook \
+    -v $NOTEBOOKS_FOLDER:/home/sbf/notebook \
     -e SHELL=/bin/bash \
     --user 1000:1000 \
     --workdir /home/sbf/notebook \
-    --name sbf_container \
+    --name $containerName \
     --hostname container \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -e DISPLAY=$display \
-    -e color_prompt=yes \
     --platform linux/amd64 \
     astromatrix/sbf_notebook:v1.0 /bin/bash
     
