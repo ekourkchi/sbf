84e4e36099bbf710ffa95d24b53b9b28fc90f4b7
ekourkchi <ekourkchi@gmail.com>
2024-12-13 12:55:49 -0800

    adding settings

---
+++ notebooks/settings.py
@@ -0,0 +1,19 @@
+
+ROOT = "/media/Data/Home/PanStarrs/Jan/HI/augment/SBF"
+
+# pysbf location
+PYSBF_PATH = ROOT + "/codes/"
+
+# configuration path
+CONFIG_FOLDER = PYSBF_PATH + 'pysbf/config/'
+
+# data path
+IN_FOLDER = ROOT + '/codes/notebooks/data/wfc3/'
+
+# output path
+OUT_FOLDER = ROOT + '/codes/notebooks/output/'
+
+# PS path
+PS_PATH = ROOT + '/codes/notebooks/data/PS/'
+
+
